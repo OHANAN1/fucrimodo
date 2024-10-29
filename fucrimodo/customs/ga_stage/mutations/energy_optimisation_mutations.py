3ab0f6a21cb59f2c3dddf19103da389610151b20
loois <louis.boehm@gmx.de>
2024-10-29 14:07:41 +0100

    Cleaned up code with results from pytest.

--- fucrimodo/customs/ga_stage/mutations/energy_optimisation_mutations.py
+++ fucrimodo/customs/ga_stage/mutations/energy_optimisation_mutations.py
@@ -2,43 +2,43 @@ from .abstract import Mutation
 from fucrimodo.core.modules import Individual
 from fucrimodo.core.utils.closest_distances_class import CustomClosestDistances
 from ase.ga import soft_mutation as ase_soft_mut
-from fucrimodo.core.utils.emt2 import EMT2 # Replace with ml model
 from ase.optimize import BFGS
 
-class RelaxationMutation(Mutation):
-    """
-    Relax the structure using the ASE BFGS optimizer.
-    """
-
-    def __init__(
-        self,
-        closest_distances: CustomClosestDistances,
-        max_steps_relaxation: int = 1000,
-        fmax: float = 0.05
-    ):
-        self.closest_distances = closest_distances
-        self.max_steps_relaxation = max_steps_relaxation
-        self.max_steps = 1
-        self.fmax = fmax
-
-    def __relax_crystal(
-        self,
-        crystal: Individual,
-        max_steps_relaxation: int,
-        fmax: float = 0.05,
-    ) -> Individual:
-        calc = EMT2()
-        crystal.calc = calc
-        dyn = BFGS(crystal, logfile="-")
-        dyn.run(fmax=fmax, steps=max_steps_relaxation)
-
-        return crystal
-
-    def perform_mutation(self, crystal: Individual) -> Individual | None:
-        offspring = self.__relax_crystal(
-            crystal, self.max_steps_relaxation, self.fmax
-        )
-        return offspring
+# Implement with ML model from MACE here
+# class RelaxationMutation(Mutation):
+#     """
+#     Relax the structure using the ASE BFGS optimizer.
+#     """
+#
+#     def __init__(
+#         self,
+#         closest_distances: CustomClosestDistances,
+#         max_steps_relaxation: int = 1000,
+#         fmax: float = 0.05
+#     ):
+#         self.closest_distances = closest_distances
+#         self.max_steps_relaxation = max_steps_relaxation
+#         self.max_steps = 1
+#         self.fmax = fmax
+#
+#     def __relax_crystal(
+#         self,
+#         crystal: Individual,
+#         max_steps_relaxation: int,
+#         fmax: float = 0.05,
+#     ) -> Individual:
+#         calc = EMT2()
+#         crystal.calc = calc
+#         dyn = BFGS(crystal, logfile="-")
+#         dyn.run(fmax=fmax, steps=max_steps_relaxation)
+#
+#         return crystal
+#
+#     def perform_mutation(self, crystal: Individual) -> Individual | None:
+#         offspring = self.__relax_crystal(
+#             crystal, self.max_steps_relaxation, self.fmax
+#         )
+#         return offspring
 
 
 class SoftMutation(Mutation):
