cdd6c8828e0b2de4006a5a2af1ff967a93f09eab
loois <louis.boehm@gmx.de>
2024-11-04 18:28:41 +0100

    Enabled configuration of analysis through fucrimodo_lab for more flexibility.

---
+++ fucrimodo/lab_template/configs/analysis/run/default.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+from fucrimodo.analysis.run_analysis import (
+    get_run_overview,
+    get_global_statistics_overview,
+    plot_global_statistics,
+    RunData
+)
+
+def main(
+    run_dir: str,
+    row: int | None = None,
+    save_dir: str | None = None,
+    show: bool = True,
+    verbose: bool = False,
+) -> None:
+    run_data = RunData(run_dir)
+
+    print("________________________________________________________")
+    print("Run Overview:")
+    print(get_run_overview(run_data).T)
+    print()
+    print("________________________________________________________")
+    print("Global Statistics Overview:")
+    global_stats_overview = get_global_statistics_overview(run_data)
+    print(global_stats_overview.T)
+    print()
+    print()
+    print("Note: To analyse crystals open ase db cli.")
+
+    # If a row is provided, only plot the selected global statistic
+    if row is not None:
+        plot_global_statistics(run_data, row)
+        if show:
+            plt.show()
+        else:
+            if save_dir is not None:
+                plt.savefig(f"{save_dir}/global_statistic_{row}.png")
+                plt.close()
+            else:
+                plt.savefig(f"global_statistic_{row}.png")
+                plt.close()
+
+    # If no row is provided, plot all global statistics
+    else:
+        for i in range(len(global_stats_overview)):
+            plot_global_statistics(run_data, i)
+            if show:
+                plt.show()
+            else:
+                if save_dir is not None:
+                    plt.savefig(f"{save_dir}/global_statistic_{i}.png")
+                    plt.close()
+                else:
+                    plt.savefig(f"global_statistic_{i}.png")
+                    plt.close()
