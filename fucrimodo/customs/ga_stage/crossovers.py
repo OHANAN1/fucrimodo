06232145b1532377544c172173b998f8dfc1be3b
loois <louis.boehm@gmx.de>
2024-11-11 21:24:10 +0100

    Improve run time by checking necessary parent conditions earlier.

--- fucrimodo/customs/ga_stage/crossovers.py
+++ fucrimodo/customs/ga_stage/crossovers.py
@@ -562,6 +562,16 @@ class CutAndSpliceCrossover(Crossover):
         parent1 = build.sort(parent1)
         parent2 = build.sort(parent2)
 
+        # Check if the parents are of the same length, this is necessary
+        # for the CutAndSplicePairing of ase
+        if len(parent1) != len(parent2):
+            return (None, None)
+
+        # Check if the parents have a minimum of 2 atoms, else the 
+        # CutAndSplicePairing is unnecessary
+        if len(parent1) < 2:
+            return (None, None)
+
         cut_and_splice_pairing = CutAndSplicePairing(
             slab=ase.Atoms(),
             blmin=self.closest_distances,
