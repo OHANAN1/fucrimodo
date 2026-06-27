263a0f40fdb6cff6dac2ca7df256c7a650146555
Louis Boehm <louis.boehm@gmx.de>
2026-06-27 17:42:01 -0300

    Add BreakCondition class to core module.

---
+++ src/fucrimodo/core/modules/break_condition.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from .population import Population
+
+
+class BreakCondition(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def check(self, population: Population, info: dict | None = None) -> bool:
+        """Method to check if the break condition is fullfilled.
+
+        :params population: Population for which the break condition should be checked.
+        :params info: Used to pass additional that are needed to check the condition.
+
+        :returns: True if break condition is fulfilled and False if not.
+        """
+        pass
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        variables = vars(self)
+        variables_str = ", ".join(f"{key}={value}" for key, value in variables.items())
+        return f"{class_name}({variables_str})"
